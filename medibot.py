54a1f8265b8f69971914a56f81a36d04a970272d
Nurcan Yaz <97188298+nurcanyaz@users.noreply.github.com>
2024-08-05 17:00:00 +0300

    connect llm

---
+++ medibot.py
@@ -0,0 +1,17 @@
+import os
+import streamlit as st
+
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains import RetrievalQA
+
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import HuggingFaceEndpoint
+
+
+DB_FAISS_PATH="vectorstore/db_faiss"
+@st.cache_resource
+def get_vectorstore():
+    embedding_model=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    db=FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+    return db
